e6b04118e15fa5082e6557d68ce62ffaebf6c2b5
DOHUNG-WINDOWS <hungdomanh95@gmail.com>
2021-03-15 00:03:33 +0700

    update about

---
+++ src/pages/ListData.js
@@ -0,0 +1,92 @@
+import React from "react"
+export const inforSocial = [
+  {
+    id: 1,
+    name: "Facebook",
+    color: "#2980b9",
+    icon: <i className="fab fa-facebook-f" />,
+    infor: "https://www.facebook.com/DoHung1995",
+    link: "https://www.facebook.com/DoHung1995",
+  },
+  {
+    id: 2,
+    name: "Gmail",
+    color: "#e74c3c",
+    icon: <i className="fa fa-envelope" />,
+    infor: "hungdomanh95@gmail.com",
+    link: "mailto:hungdomanh95@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Facebook",
+    color: "#2ecc71",
+    icon: <i className="fa fa-mobile-alt" />,
+    infor: "037 7711 455",
+    link: "",
+  },
+  {
+    id: 4,
+    name: "Github",
+    color: "#2ecc71",
+    icon: <i class="fab fa-github"></i>,
+    link: "https://github.com/hungdomanh95",
+  },
+]
+export const inforMySelfLeft = [
+  {
+    id: 1,
+    name: "Birthday",
+    icon: <i class="fa fa-calendar"></i>,
+    infor: "20-12-1995",
+  },
+  {
+    id: 2,
+    name: "Location",
+    icon: <i class="fa fa-map-marker-alt"></i>,
+    infor: "Ho Chi Minh City, VietNam",
+  },
+  {
+    id: 3,
+    name: "Email",
+    icon: <i class="fa fa-envelope"></i>,
+    infor: "hungdomanh95@gmail.com",
+  },
+]
+export const inforMySelfRight = [
+  {
+    id: 1,
+    name: "Born and bought up in",
+    images: "images/location.png",
+    content: "Gia Lai Province, VietNam",
+  },
+  {
+    id: 2,
+    name: "Love Soft drinks",
+    images: "images/coffee.png",
+    content: "Soft drinks + Me = Happiness",
+  },
+  {
+    id: 3,
+    name: "Socially Awkward",
+    images: "images/meeting.png",
+    content: "At times",
+  },
+  {
+    id: 4,
+    name: "Love Riding",
+    images: "images/motorcycle.png",
+    content: "Biker for life",
+  },
+  {
+    id: 5,
+    name: "Self Taught Programmer",
+    images: "images/web.png",
+    content: "Thanks to the Web Resources",
+  },
+  {
+    id: 6,
+    name: "Pursued B.Tech in",
+    images: "images/graduation.png",
+    content: "HCM City University Of Transport",
+  },
+]
---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import InforLeft from './about/InforLeft'
+import InforRight from './about/InforRight'
+
+const about = () => {
+    return (
+        <div className='about'>
+           <InforLeft/>
+           <InforRight/>
+           
+        </div>
+    )
+}
+
+export default about
